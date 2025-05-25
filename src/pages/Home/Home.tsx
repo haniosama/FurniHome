2f39f39c93ade9810fbe218d86fcd2180330c050
hani osama <haniosama124@gmail.com>
2025-05-25 11:01:19 +0300

    solve some bug

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import Footer from "../../component/footer";
 import Hero from "../../component/Hero";
-import Navbar from "../../component/navbar";
+import Navbar from "../../component/Navbar";
 import Product from "../../component/Product";
 
 const Home = () => {
