2a04fb2fdb4e9eed1a6ffda2e2d106b089dc728f
Youssef <ezzatyousser79@gmail.com>
2025-05-25 13:31:09 +0300

    update UI of setting page

--- src/pages/Setting/Setting.tsx
+++ src/pages/Setting/Setting.tsx
@@ -111,7 +111,7 @@ const Setting: React.FC = () => {
     if (userInfo.avatarFile) {
       formData.append('avatar', userInfo.avatarFile);
     } else {
-      formData.append('oldAvatar', userInfo.avatar || 'profile.png');
+      formData.append('oldAvatar', userInfo.avatar || 'Ronaldo.jpg');
     }
     formData.append('username', userInfo.username);
     formData.append('phone', userInfo.phone);
@@ -149,9 +149,9 @@ const Setting: React.FC = () => {
         className="relative text-3xl md:text-4xl font-bold mb-12 text-[#0058AA]
                   inline-block
                   after:content-[''] after:absolute after:-bottom-3 after:left-0 
-                  after:w-full after:h-1 after:bg-[#FBD913] after:opacity-80
+                  after:w-full after:h-1 after:bg-[#FBD913] after:opacity-100
                   before:content-[''] before:absolute before:-bottom-5 before:left-1/4 
-                  before:w-1/2 before:h-1 before:bg-[#0058AA] before:opacity-50"
+                  before:w-1/2 before:h-1 before:bg-[#0058AA] before:opacity-80"
       >
         Personal Settings
       </h2>
