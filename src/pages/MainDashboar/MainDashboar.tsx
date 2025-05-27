f7d30ffa5b6fcb80310b1bbcea5003ff8d84cac1
haniosama <78438996+haniosama@users.noreply.github.com>
2025-05-27 03:28:19 +0300

    Update MainDashboar.tsx

--- src/pages/MainDashboar/MainDashboar.tsx
+++ src/pages/MainDashboar/MainDashboar.tsx
@@ -2,18 +2,18 @@ import { MdOutlineAttachMoney } from "react-icons/md"
 import ChartDashboard from "../../component/ChartDashboard"
 import CusomterTableMainDashboard from "../../component/CusomterTableMainDashboard"
 import ChartCricleDashboard from "../../component/ChartDashboardCricle"
-import CardDashboard from "../../component/CardDashboard"
+// import CardDashboard from "../../component/CardDashboard"
 
 
 const MainDashboar = () => {
 
     return (
         <div className="mt-5">
-                <div className="flex gap-4 justify-center lg:justify-around flex-wrap">
+{/*                 <div className="flex gap-4 justify-center lg:justify-around flex-wrap">
                     <CardDashboard header="Total Sales" icon={<MdOutlineAttachMoney className="text-lg"/>} ratio={3.2} quantity={240000}/>
                     <CardDashboard header="Total Orders" icon={<MdOutlineAttachMoney className="text-lg"/>} ratio={12.2} quantity={300}/>
                     <CardDashboard header="Customers" icon={<MdOutlineAttachMoney className="text-lg"/>} ratio={13.2} quantity={50}/>
-                </div>
+                </div> */}
                 <div className="min-h-[430px] bg-white mt-5 rounded-2xl">
                     <h2 className='text-xl font-semibold ml-4 pt-4'>Report</h2>
                     <ChartDashboard/>
